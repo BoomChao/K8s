ba5349cad255ef905a59aaf319c1f95b0256ce9f
jinchaozhu <jinchaozhu@tencent.com>
2025-07-24 14:50:53 +0800

    feat: add pod informer

--- controller/informer/main.go
+++ controller/informer/main.go
@@ -1,5 +1,48 @@
 package main
 
-func main() {
+import (
+	"flag"
+	"path/filepath"
+	"time"
 
+	"k8s.io/client-go/informers"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/cache"
+	"k8s.io/client-go/tools/clientcmd"
+	"k8s.io/client-go/util/homedir"
+)
+
+func main() {
+	var kubeconfig *string
+	if home := homedir.HomeDir(); home != "" {
+		kubeconfig = flag.String("kubeconfig", filepath.Join(home, ".kube", "config"), "")
+	}
+	flag.Parse()
+
+	config, err := clientcmd.BuildConfigFromFlags("", *kubeconfig)
+	if err != nil {
+		panic(err)
+	}
+
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		panic(err)
+	}
+
+	factory := informers.NewSharedInformerFactory(clientset, time.Hour*24)
+
+	podInformer := factory.Core().V1().Pods().Informer()
+
+	podInformer.AddEventHandler(cache.ResourceEventHandlerFuncs{
+		AddFunc:    func(obj interface{}) {},
+		UpdateFunc: func(oldObj, newObj interface{}) {},
+		DeleteFunc: func(obj interface{}) {},
+	})
+
+	stop := make(chan struct{})
+	defer close(stop)
+
+	factory.Start(stop)
+
+	select {}
 }
